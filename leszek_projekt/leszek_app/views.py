2d8cc308dd697bac6311037c17179fd8ceccd7d5
Example user <you@example.com>
2025-02-10 14:11:58 +0100

    v0.1 - landing page - exam page (template) - basic setup
    Co-authored-by: Hubert Drozd <hdrozd@zsat-ropczyce.pl>

--- leszek_projekt/leszek_app/views.py
+++ leszek_projekt/leszek_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    classifications = ["Inf.02", "Inf.03", "Ele.02", "Ele.03", "SPC.02", "SPC.03", "ROL.02", "ROL.03"]
+    context = {"classifications": classifications}
+    return render(request, "landing.html", context)
+
+def exam(request):
+    examCode = request.GET.get("code")
+    return render(request,'exam.html',{"code": examCode})
+    pass
