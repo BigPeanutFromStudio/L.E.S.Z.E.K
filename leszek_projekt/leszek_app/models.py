f5c9b5b9c314a8359096df64dc36e26db7c9439a
Example user <you@example.com>
2025-03-31 14:40:38 +0200

    v0.3 - Implemented a basic exam system - Improved UI (still far from final)

--- leszek_projekt/leszek_app/models.py
+++ leszek_projekt/leszek_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from typing import Dict,Any
 
 class Code(models.Model):
     codeName = models.CharField(max_length=5000)
@@ -8,6 +9,7 @@ class Code(models.Model):
 
 # Create your models here.
 class Question(models.Model):
+    id = models.AutoField(primary_key=True)
     question = models.CharField(max_length=10000)
     answer_a = models.CharField(max_length=5000)
     answer_b = models.CharField(max_length=5000)
@@ -16,6 +18,7 @@ class Question(models.Model):
     correct_answer = models.CharField(null=True,blank=True,max_length=5000)
     media = models.CharField(null=True,blank=True, max_length=5000) #FilePathField
     code_ID = models.ForeignKey(Code,on_delete=models.DO_NOTHING)
+    
     class Meta:
         unique_together = ('question', 'media', 'code_ID')
     def __str__(self):
@@ -26,6 +29,17 @@ class Question(models.Model):
             mediaRef = '0'
         return f'{self.code_ID}_{mediaRef}'
 
+    def prepare(self) -> Dict[str,Any]:
+        return {
+            'id':self.id,
+            'question':self.question,
+            'answer_a':self.answer_a,
+            'answer_b':self.answer_b,
+            'answer_c':self.answer_c,
+            'answer_d':self.answer_d,
+            'media':self.media
+        }
+        ...
 
 class QuestionApplication(models.Model):
     questionID = models.ForeignKey(Question,on_delete=models.DO_NOTHING)
--- leszek_projekt/leszek_app/views.py
+++ leszek_projekt/leszek_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from random import sample
 from .models import Question, Code
+from json import dumps
 from django.core.serializers import serialize
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, HttpResponseNotFound
@@ -25,8 +26,8 @@ def getExam(request: HttpRequest, examCode):
         questions = []
         numbers = sample(range(1, count + 1), 40)
         for n in numbers:
-            questions.append(Question.objects.get(id=n))
-        return HttpResponse(serialize("json", questions))
+            questions.append(Question.objects.get(id=n).prepare())
+        return HttpResponse(dumps(questions))
     else:
         # we know it should be 404 but ... it is easier
         return HttpResponse(serialize("json",[]))
