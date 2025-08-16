65a2d75cc553958a79b2a68417619debbd870e89
BigPeanut <64210078+BigPeanutFromStudio@users.noreply.github.com>
2025-08-16 19:16:24 +0200

    v1.0.1
    - Updated ReadMe.md
    - Replaced example questions with correctly formatted ones
    - Added a point indicator to the exam results
    - Landing page displays only available questions
    - Misc.
    
    ---------
    
    Co-authored-by: HD16K <HD16K@users.noreply.github.com>

--- ReadMe.md
+++ ReadMe.md
@@ -11,55 +11,80 @@ Jeśli znajdzie się ktoś chętny na długotrwałe i aktywne wspieranie/przeję
 ## Przygotowanie dla developerów
 
 Projekt został stworzony przy użyciu pythona >=3.12.3 oraz biblioteki [Django](https://www.djangoproject.com/ 'Dokumentacja Django').
+![landing dark](./screenshots/landing-dark.png)
+
+## Omówienie
+
+L.E.S.Z.E.K jest systemem mającym na celu pomoc uczniom przystępującym do egzaminów zawodowych. Głównie skupia się on na ZSAT w Ropczycach jednak jest on otwarty dla każdego. Poniżej znajdują się instrukcje dla ambitnych i pełnych energii (jeszcze) programistów, którzy chcą się przyczynić do projektu. Repozytorium może posłużyć również jako materiał edukacyjny/treningowy dla tych, którzy chcą nauczyć się pracować i przyczyniać się do publicznych projektów.
+
+Jeśli znajdzie się ktoś chętny na długotrwałe i aktywne wspieranie/przejęcie projektu, dajcie nam znać (najlepiej przez github discussions), a przedyskutujemy wszelkie szczegóły.
+
+## Przygotowanie dla developerów
+
+Projekt został stworzony przy użyciu pythona >=3.12.3 oraz biblioteki [Django](https://www.djangoproject.com/ 'Dokumentacja Django').
 
 Systemu należy używać poprzez wirtualne środowisko pythona. Po sklonowaniu repozytorium należy jednorazowo stworzyć to środowisko.
 Aby je stworzyć postępuj wedle następujących kroków.
 
 1. Stwórz wirtualne środowisko (nazwę "leszek" można zastąpić dowolną nazwą)
 
+
 ```pwsh
 $  py -m venv leszek
 ```
 
-2. Zainstaluj wymagane biblioteki poprzez
-
-```pwsh
-$  pip install -r requirements.txt
-```
-
-## Jak zacząć
-
 Wszystkie akcje wykonuj używając wirtualnego środowiska pythona. Aby do niego wejść wykonaj poniższe kroki.
 
-1. Jeśli używasz powershell użyj
+2. Jeśli używasz powershell użyj
 
 ```pwsh
 $ Set-ExecutionPolicy Unrestricted -Scope Process
 ```
 
-2. Następnie uruchom skrypt "activate" znajdujący się w folderze L.E.S.Z.E.K
+3. Następnie uruchom skrypt "activate" znajdujący się w folderze L.E.S.Z.E.K
 
 ```pwsh
 $ .\leszek\Scripts\activate
 ```
 
-3. Przeprowadź migracje aby baza danych została zinicjalizowana
+4. Zainstaluj wymagane biblioteki poprzez
+
+```pwsh
+$  py -m pip install -r requirements.txt
+```
+
+5. Przeprowadź migracje aby baza danych została zinicjalizowana
 
 ```pwsh
 $ py manage.py migrate
 ```
 
-4. Zaimportuj dane, najpierw importując kody z pliku `\import_data\codes.json`, a następnie przykładowe pytania z pliku `\import_data\questions_export.json` lub pytania z aktualnej bazy danych.
+1. Zaimportuj dane, najpierw importując kody z pliku [.\import_data\codes.json](leszek_projekt\import_data\codes.json), a następnie przykładowe pytania z pliku [.\import_data\example_questions.json](leszek_projekt\import_data\example_questions.json) [(jak importować niżej)](#jak-zaimportować-dane-z-systemu-aspekt) lub pytania z aktualnej bazy danych.
+
+```pwsh
+$ py manage.py loaddata .\import_data\codes.json
+```
+
+7. Stwórz super usera
+
+```pwsh
+$ py manage.py createsuperuser
+```
 
 ## Jak włączyć serwer
 
+UWAGA: Poniższe kroki należy wykonywać będąc w wirtualnym środowisku pythona (aka. venv).
+
 1. Przejdź do folderu leszek_projekt
+
 
 ```pwsh
 $ cd .\leszek_projekt
 ```
 
+
 2. Aby uruchomić serwer użyj komendy:
+
 
 ```pwsh
 $ py manage.py runserver
@@ -69,7 +94,9 @@ $ py manage.py runserver
 
 1. Do folderu "leszek_projekt\import_data" przenieś plik "questions_import.json" otrzymany po użyciu A.S.P.E.K.T
 
+
 2. Użyj komendy:
+
 
 ```pwsh
 $ py manage.py loaddata .\import_data\questions_export.json
@@ -77,9 +104,18 @@ $ py manage.py loaddata .\import_data\questions_export.json
 
 Lub użyj ✨panelu admina✨ ;)
 
-![admin panel](./screenshots/admin.png)
+![admin import panel](./screenshots/admin_import.png)
 
 Następnie do folderu "leszek_projekt\static_files\media" przenieś wszystkie pliki otrzymane po użyciu A.S.P.E.K.T (powinny znajdować się w folderze "media")
+
+---
+
+> ### Ta platforma nie jest powiązana z Centralną Komisją Egzaminacyjną (CKE).
+>
+> Jej celem jest edukacja i ćwiczenie umiejętności egzaminacyjnych w bezpiecznym i nieformalnym środowisku.
+> Wszystkie treści mają charakter pomocniczy i nieoficjalny
+
+---
 
 ## Contributing
 
--- leszek_projekt/leszek_app/views.py
+++ leszek_projekt/leszek_app/views.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from django.shortcuts import render, redirect
 from .models import Question, Code, QuestionApplication
+from datetime import datetime
+from django.shortcuts import render, redirect
+from .models import Question, Code, QuestionApplication
 from json import dumps, loads
+from django.db.models import Count, F
 from django.db.models import Count, F
 from django.core.serializers import serialize
 from django.http.request import HttpRequest
@@ -11,10 +15,9 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
-def landing(request: HttpRequest):
-    if (request.user.is_authenticated): 
-        applied_question_ids = QuestionApplication.objects.values("question_id")
-        classifications_quantities = list(Question.objects.filter(correct_answer__isnull=True)
+def _prepare(bool):
+    applied_question_ids = QuestionApplication.objects.values("question_id")
+    return list(Question.objects.filter(correct_answer__isnull=bool)
     .exclude(pk__in=applied_question_ids)
     .annotate(
         code_name=F("code_id__code_name")
@@ -22,17 +25,23 @@ def landing(request: HttpRequest):
     .values("code_name")
     .annotate(count=Count("id")) 
     .order_by("code_name"))
+    
+
+def landing(request: HttpRequest):
+    if (request.user.is_authenticated): 
+        classifications_quantities = _prepare(True)
     else:
-        classifications_quantities = list(Code.objects.annotate(count=Count('question')).values('code_name', 'count'))
+        classifications_quantities = _prepare(False)
     print(classifications_quantities)
     return render(request, "landing.html", {"classifications": classifications_quantities})
 
+@csrf_exempt
 @csrf_exempt
 def exam(request: HttpRequest):
     examCode = request.GET.get("code")
     numberOfQuestions = int(request.GET.get("nr"))
     numberOfQuestions = max(1, min(40, numberOfQuestions))
-    questions = list(map(lambda q: q.prepare(), Question.objects.filter(code_id=Code.objects.get(code_name=examCode)).order_by("?")[:numberOfQuestions]))
+    questions = list(map(lambda q: q.prepare(), Question.objects.filter(code_id=Code.objects.get(code_name=examCode)).filter(correct_answer__isnull=False).order_by("?")[:numberOfQuestions]))
     
     context = {"examCode": examCode, "questions": dumps(questions), "numberOfQuestions": numberOfQuestions}
     print(context)
@@ -40,8 +49,15 @@ def exam(request: HttpRequest):
 
 
 def post_exam_results(request: HttpRequest):
+def post_exam_results(request: HttpRequest):
     questions = loads(request.body)["questions"]
     score = 0
+    for i, question in enumerate(questions):
+        correct_answer = Question.objects.filter(id=question["id"]).values("correct_answer")[0]["correct_answer"]
+        questions[i]["correct_answer"] = correct_answer
+        if question["selected_answer"] == correct_answer: 
+            score += 1
+        
     for i, question in enumerate(questions):
         correct_answer = Question.objects.filter(id=question["id"]).values("correct_answer")[0]["correct_answer"]
         questions[i]["correct_answer"] = correct_answer
@@ -53,8 +69,13 @@ def post_exam_results(request: HttpRequest):
     request.session['date'] = datetime.now().strftime("%d/%m/%Y %H:%M")
 
 
+    request.session['questions'] = questions
+    request.session['date'] = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+
     return HttpResponse(serialize("json", [])) 
 
+def display_exam_results(request: HttpRequest):
 def display_exam_results(request: HttpRequest):
     if request.session.has_key("score"):
         results = dumps({"msg": "Success", "questions": request.session['questions'], "score": request.session['score'],"date":request.session['date']})
@@ -110,7 +131,71 @@ def login_view(request):
             if "next" in request.POST:
                 return redirect(request.POST.get('next'))
             return redirect("/")
+        results = dumps({"msg": "Success", "questions": request.session['questions'], "score": request.session['score'],"date":request.session['date']})
     else:
+        results = {"msg": "Brak wyników ostatniego egzaminu"}
+
+    return render(request, "results.html", {"results": results})
+
+
+
+@login_required(login_url="/login/")
+def application_form_view(request, code):
+    unanswered_questions = Question.objects.filter(code_id=Code.objects.get(code_name=code),correct_answer__isnull=True).all()
+    random_q = unanswered_questions.exclude(question__in=QuestionApplication.objects.all().values('question_id__question')).order_by('?').first()
+    return render(request, "application_form.html", {"code": code,"question":random_q,"question_json":dumps(random_q.prepare())})
+
+@login_required(login_url="/login/")
+def save_correct_answer(request: HttpRequest):
+    question = Question.objects.get(id = loads(request.body)["question"]["id"])
+    correct_answer = loads(request.body)["question"]["correct_answer"]
+
+    question_aplication = QuestionApplication(question_id = question,correct_answer = correct_answer,user_id=request.user,sent_at = datetime.now())
+    question_aplication.save()
+    return JsonResponse({"success": True})
+
+
+def cke_login(request: HttpRequest):
+    return render(request, "cke/login.html")
+def cke_exam(request: HttpRequest):
+    return render(request, "cke/cke_exam.html",{"server_url":"http://127.0.0.1:8000/"})
+def cke_answer(request: HttpRequest):
+    return render(request, "cke/answer.html",{"server_url":"http://127.0.0.1:8000/"})
+def cke_exam_instruction(request: HttpRequest):
+    return render(request, "cke/cke_exam_instrukcja.html",{"server_url":"http://127.0.0.1:8000/"})
+
+# def register_view(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             login(request, form.save())
+#             if "next" in request.POST:
+#                 return redirect(request.POST.get('next'))
+#             return redirect("/")
+#     else:
+#         form = UserCreationForm()
+#     return render(request, "user/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            if "next" in request.POST:
+                return redirect(request.POST.get('next'))
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, "user/login.html", {"form": form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/")
+
+def privacy_policy(request):
+    return render(request,"privacy_policy.html",)
         form = AuthenticationForm()
     return render(request, "user/login.html", {"form": form})
 
